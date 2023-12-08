aa69ca47b9d31742456374a635c9e48edcd7b8ac
Samuel F. Correa <sam77freirecorrea@gmail.com>
2023-12-08 18:09:01 -0400

    pipipopo

--- projectone/projectone/Controllers/HomeController.cs
+++ projectone/projectone/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace projectone.Controllers
             return View();
         }
 
+        public IActionResult fotos()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
